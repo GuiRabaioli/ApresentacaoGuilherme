4e5183cf6f437c68ef1629f8f92829ee74f92cbc
Guilherme Rabaioli <guirabaioli234@gmail.com>
2022-08-01 13:59:51 -0400

    style:arrumando top

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.addEventListener("scroll", () => {
     mountains_behind.style.top = value * 0.5 + "px"
     mountains_front.style.top = value * 0 + "px"
     text.style.marginRight = value * 4 + "px"
-    text.style.marginTop = value * 1.1 + "px"
+    text.style.marginTop = value * 0.2 + "px"
     btn.style.marginTop = value * 1.1 + "px"
     header.style.top = value * 0.5 + "px"
 
